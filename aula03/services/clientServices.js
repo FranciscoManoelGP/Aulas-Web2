2ba237d7bde671d2e4c5d1d69afda1d3e9103a14
Francisco Manoel <f.manoel.neto312@gmail.com>
2023-09-22 20:14:06 -0300

    refactor(clientServices): add middleware functions

--- aula03/services/clientServices.js
+++ aula03/services/clientServices.js
@@ -1,13 +1,33 @@
 const fs = require("fs");
 
+let clientList = [];
+
+function clientListLoad() {
+    try {
+        clientList = JSON.parse(fs.readFileSync("../clientList.json"));
+    } catch (error) {
+        clientList = [];
+    }
+}
+
+function clientListIsEmpty() {
+    if (clientList.length === 0) {
+        clientListLoad();
+    }
+
+    return clientList;
+}
+
 function getAllClients() {
-    return JSON.parse(fs.readFileSync("../clientList.json"));
+    const result = clientListIsEmpty();
+
+    return result;
 }
 
 function getClientById(id) {
-    return JSON.parse(
-        fs.readFileSync("../clientList.json")
-    ).find(client => client.id === id);
+    const result = clientListIsEmpty().find(client => client.id === id);
+    
+    return result;
 }
 
 module.exports = {
